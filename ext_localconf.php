0b5b9e392f5aeb34a98f4cfeab8a3c86233c52bf
mhirdes <hirdes@clickstorm.de>
2018-05-17 10:35:18 +0200

    [FEATURE] allow TypoScript override

--- ext_localconf.php
+++ ext_localconf.php
@@ -12,3 +12,6 @@ $signalSlotDispatcher->connect(
     'manipulateForm',
     FALSE
 );
+
+$GLOBALS['TYPO3_CONF_VARS']['SYS']['locallangXMLOverride']['EXT:powermail/Resources/Private/Language/locallang.xlf'][]
+    = 'EXT:cs_powermail_gdpr/Resources/Private/Language/locallang.xlf';
