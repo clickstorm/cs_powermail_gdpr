98fdcf43e3b1aba1771c7c18ff68580111577572
Alexander König <koenig@clickstorm.de>
2023-04-26 11:34:19 +0200

    [TASK] reverts changes from last commit + cleanup + up version to 3.1.3

--- Classes/Domain/Validator/GdprAcceptedValidator.php
+++ Classes/Domain/Validator/GdprAcceptedValidator.php
@@ -47,9 +47,9 @@ class GdprAcceptedValidator extends AbstractValidator
         $result = GeneralUtility::makeInstance(Result::class);
 
         // throw error
-        if (!$mail->getForm()->isTxCspowermailgdprHidden()) {
+        if(!$mail->getForm()->isTxCspowermailgdprHidden()) {
             $params = GeneralUtility::_GPmerged('tx_powermail_pi1');
-            if (empty($params['field']['tx_cspowermailgdpr_accepted']) && !$mail->isTxCspowermailgdprAccepted()) {
+            if(!$params['field']['tx_cspowermailgdpr_accepted'] && !$mail->isTxCspowermailgdprAccepted()) {
                 $errorMarker = LocalizationUtility::translate('tx_cspowermailgdpr.checkbox.marker', 'cs_powermail_gdpr') . ':';
                 $result->addError(new Error($errorMarker, 123009282, [], 'Error tx_cspowermailgdpr'));
             }
