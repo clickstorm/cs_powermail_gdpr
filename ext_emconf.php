fc07204062f375ff11b631877798d7806ad4a600
mhirdes <Marc_Hirdes@gmx.de>
2023-05-02 11:43:32 +0200

    Update ext_emconf.php

--- ext_emconf.php
+++ ext_emconf.php
@@ -8,10 +8,10 @@ $EM_CONF[$_EXTKEY] = [
     'author_email' => 'hirdes@clickstorm.de',
     'author_company' => 'clickstorm GmbH',
     'state' => 'beta',
-    'version' => '3.1.4',
+    'version' => '4.0.0-dev',
     'constraints' => [
         'depends' => [
-            'typo3' => '11.5.0-11.5.99'
+            'typo3' => ''
         ],
         'conflicts' => [
         ],
