d06e5f181d76f7f2254282215dae2da84528c72c
Alexander König <koenig@clickstorm.de>
2023-10-10 11:38:13 +0200

    [TASK] remove . from property name + add ViewHelper isChecked for overview pages back button

---
+++ Classes/ViewHelpers/IsGdprCheckboxCheckedViewHelper.php
@@ -0,0 +1,41 @@
+<?php
+namespace Clickstorm\CsPowermailGdpr\ViewHelpers;
+
+use TYPO3Fluid\Fluid\Core\Rendering\RenderingContextInterface;
+use TYPO3Fluid\Fluid\Core\ViewHelper\AbstractViewHelper;
+
+/**
+ * ViewHelper for php str_replace function
+ */
+class IsGdprCheckboxCheckedViewHelper extends AbstractViewHelper
+{
+    protected $escapeOutput = false;
+
+    public function initializeArguments(): void
+    {
+        parent::initializeArguments();
+        $this->registerArgument(
+            'property',
+            'string',
+            'Property name of the powermail gdpr field',
+            true
+        );
+    }
+
+    /**
+     * @param array $arguments
+     * @param \Closure $renderChildrenClosure
+     * @param RenderingContextInterface $renderingContext
+     * @return mixed
+     */
+    public static function renderStatic(
+        array $arguments,
+        \Closure $renderChildrenClosure,
+        RenderingContextInterface $renderingContext
+    ) {
+        if(isset($_POST['tx_powermail_pi1']['field'][$arguments['name']])) {
+            $checked = $_POST['tx_powermail_pi1']['field'][$arguments['name']][0] ? true : false;
+        }
+        return $checked ?? false;
+    }
+}
