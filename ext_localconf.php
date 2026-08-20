8c34473f20770f6790369bbf0296c0ee48456fa8
mhirdes <hirdes@clickstorm.de>
2026-08-20 16:01:26 +0200

    [TASK] language label overrides

--- ext_localconf.php
+++ ext_localconf.php
@@ -3,8 +3,13 @@
 defined('TYPO3') || die();
 
 (function () {
-    $GLOBALS['TYPO3_CONF_VARS']['SYS']['locallangXMLOverride']['EXT:powermail/Resources/Private/Language/locallang.xlf'][]
-        = 'EXT:cs_powermail_gdpr/Resources/Private/Language/locallang.xlf';
+    if ((new \TYPO3\CMS\Core\Information\Typo3Version())->getMajorVersion() >= 14) {
+        $GLOBALS['TYPO3_CONF_VARS']['SYS']['localization']['resourceOverrides']['EXT:powermail/Resources/Private/Language/locallang.xlf']
+            = 'EXT:cs_powermail_gdpr/Resources/Private/Language/locallang.xlf';
+    } else {
+        $GLOBALS['TYPO3_CONF_VARS']['SYS']['locallangXMLOverride']['EXT:powermail/Resources/Private/Language/locallang.xlf'][]
+            = 'EXT:cs_powermail_gdpr/Resources/Private/Language/locallang.xlf';
+    }
 
     // use XClasses instead of sublcasses
     $GLOBALS['TYPO3_CONF_VARS']['SYS']['Objects'][\In2code\Powermail\Domain\Model\Mail::class] = [
